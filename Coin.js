0f1f3348d3d99aab2046a33131a2c5e2c89fe0c9
Árni Þór Sörensen <arni@hugsnilld.is>
2021-11-12 09:02:35 +0000

    Coin fix

--- Coin.js
+++ Coin.js
@@ -15,12 +15,18 @@
 function Coin(descr) {
   // Common inherited setup logic from Entity
   Character.call(this, descr);
+
   this.scale = 1;
   this.SPRITE_WIDTH = 16 * this.scale;
   this.SPRITE_HEIGHT = 16 * this.scale;
 
   //Sprite stuff
-  this.sprite = g_sprites.coins;
+  this.sprite = new Sprite(g_images.coins,12,4,16,16)
+  this.sprite.animations = {
+    GREEN: [12,13,14,15,16,17,18,19,20,21,22,23],
+    GOLD: [ 24,25,26,27,28,29,30,31,32,33,34,35]
+  }
+
   this.floor = 0;
   this.onGround = false;
   this.collider = new Collider({
@@ -58,6 +64,7 @@ Coin.prototype.update = function (du) {
     if (this.lifeSpan === 20000 / NOMINAL_UPDATE_INTERVAL) {
         this.sprite.animation = this.coinType;
         this.sprite.scale = (this.coinType === 'GREEN' ? 1 : 2)
+        this.collider.offsetY = (this.coinType === 'GOLD' ? this.SPRITE_HEIGHT/2 : 0)
     }
 
     this.lifeSpan -= du;
