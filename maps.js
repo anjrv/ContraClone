f757822ce12596db9e492041d654502fa14da8bf
jaykaytherobot <johannekarisolmundarson@gmail.com>
2021-11-15 10:38:20 +0000

    hotfix for cors error

--- maps.js
+++ maps.js
@@ -1,40 +1,32 @@
 var levels = [];
 var currentLevel = -1;
 
-const levelPaths = [
-  "/Maps/Level 1 Caves.json",
-  "/Maps/Level 2 Alien Base.json"
-];
+// const levelPaths = [
+//   "https://notendur.hi.is/~jks21/Contra/Level%201%20Caves.jsonMaps/Level%201%20Caves.json",
+//   "https://notendur.hi.is/~jks21/Contra/Level%201%20Caves.json/Level%202%20Alien%20Base.json"
+// ];
+
+// const loadLevels = async function() {
+//   let id = 1;
+//   for (const path of levelPaths) {
+//     const result = await fetch(path)
+//       .then(res => res.json())
+//       .finally(data => data)
+//       .catch((e) => console.error("Failed to load levels: " + e))
+//     result.id = id;
+//     result.tilesize = p_realSize * 0.4;
+//     levels.push(result);
+//     id++;
+//   };
+// }
 
 const loadLevels = async function() {
   let id = 1;
-  for (const path of levelPaths) {
-    const result = await fetch(path)
-      .then(res => res.json())
-      .finally(data => data)
-      .catch((e) => console.error("Failed to load levels: " + e))
-    result.id = id;
-    result.tilesize = p_realSize * 0.4;
-    levels.push(result);
-    id++;
-  };
+  level1.id = id;
+  level1.tilesize = p_realSize * 0.4;
+  levels.push(level1);
+  id++;
+  level2.id = id;
+  level2.tilesize = p_realSize * 0.4;
+  levels.push(level2);
 }
-
-// 'NO', 'NP', 'NQ'
-// 'OO', 'OP', 'OQ' doorway
-
-// 'nV', 'nW',
-// 'oV', 'oW',
-// 'pV', 'pW', small tree
-
-// 'lT', 'lU'
-// 'mT', 'mU'
-// 'nT', 'nU'
-// 'oT', 'oU'
-// 'pT', 'pU' big tree
-
-// 'EK', 'EL', 'EM', 'EN', 'EK'
-// 'FK', 'FL', 'FM', 'FN', 'FK'
-// 'GK', 'GL', 'GM', 'GN', 'GK'
-// 'HK', 'HL', 'HM', 'HN', 'HK'
-
